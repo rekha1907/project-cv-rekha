0e66c70379e7d07904d244224dc8a8c7d7b329ed
rekha1907 <rekhawidyaandini@gmail.com>
2024-04-05 21:12:50 +0700

    tugas1

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>
+      What do you think about me?
+      <a href="https://www.tiktok.com/@viterbo9?lang=en" target="_blank" rel="noopener">TIK TOK</a> +
+      <a href="https://www.instagram.com/?hl=en" target="_blank" rel="noopener">IG</a>. What's next?
+    </h3>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,71 @@
+<script setup lang="ts">
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>INTRODUCING</template>
+
+    Hello semua perkenalkan nama ku Rekha Widya Andini, berasal dari Dumai. Dumai merupakan kota pesisir timur provinsi Riau. Sekarang Dumai dipimpn seorang Walikota yang baik dan sholeh. Jika teman-teman ingin berteman dengan pak walkot, bisa kelian follow instagram beliau
+    <a href="https://www.instagram.com/h.paisal_official/?hl=en" target="_blank" rel="noopener">INSTRAGRAM</a>
+    Nama walikota Dumai bernama H.PAISAL. Saya mempunyai beberapa hobi seperi membaca novel, menonton drakor, fangirl.
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>DUMAI</template>
+
+    Kota Dumai adalah sebuah kota di provinsi Riau, Indonesia, sekitar 201 km dari Kota Pekanbaru. Kota Dumai adalah kota dengan wilayah administrasi terluas kedua di Indonesia berdasarkan statusnya sebagai kotamadya, setelah Kota Palangka Raya. Kota ini berawal dari sebuah dusun kecil di pesisir timur Provinsi Riau.
+    Kota Dumai merupakan hasil pemekaran dari Kabupaten Bengkalis. Diresmikan sebagai kota pada 20 April 1999, dengan UU No. 16 tahun 1999 tanggal 20 April 1999 setelah sebelumnya sempat menjadi kota administratif (kotif) di dalam Kabupaten Bengkalis. Pada awal pembentukannya, Kota Dumai hanya terdiri atas 3 kecamatan, 13 kelurahan dan 9 desa dengan jumlah penduduk hanya 15.699 jiwa dengan tingkat kepadatan 83,85 jiwa/km2.
+    <a href="https://www.instagram.com/sepdum/?hl=en" target="_blank" rel="noopener">Informasi seputar Dumai</a>. 
+
+    <br />
+
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <EcosystemIcon />
+    </template>
+    <template #heading>SOCIAL MEDIA</template>
+
+    Berikut sosial media yang saya mainkan
+    <a href="https://www.tiktok.com/@viterbo9?lang=en" target="_blank" rel="noopener">Tik Tok</a>,
+    <a href="https://www.instagram.com/?hl=en" target="_blank" rel="noopener">Instagram</a>,
+    <a href="https://twitter.com/Rekha39339386" target="_blank" rel="noopener">X</a>, and
+    <a href="https://weverse.io/more" target="_blank" rel="noopener">Weverse</a>. Bagi yang tidak tau weverse itu apa. weverse merupakan aplikasi yang di buat oleh BIG HIT LABELS agar mempermudah interaksi fans ke IDOL. Dengan mengirim pesan-pesan ke Idol dan akan di notice oleh Idol secara gratis atau free. Weverse awalnya hanya untuk artis-artis BIG HIT LABELS, sePerti BTS, TXT, dll. Tapi berjalannya waktu sekarang weverse digunakan untuk semua IDOL. 
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <CommunityIcon />
+    </template>
+    <template #heading>IDOLA</template>
+
+    Saya mempunyai beberapa idola, sebagai kpopers bakal banyak sekali idol yang mereka suka. salah satu idol sayang saya suka dari dulu sampai sekarang adalah BTS, saya mengidolakan mereka sejak 2019 sampai sekarang yang artinya saya sudah suka sama mereka sejak 5 tahun lalu.
+    <a href="https://www.tiktok.com/search?lang=en&q=bts&t=1712319936526" target="_blank" rel="noopener">Tik Tok</a> 
+    <a href="https://www.youtube.com/@BTS" target="_blank" rel="noopener">You Tube</a>
+    <a href="https://www.instagram.com/bts.bighitofficial/?hl=en" target="_blank" rel="noopener">Instagram</a> merupakan beberapa sosmed BTS.
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <SupportIcon />
+    </template>
+    <template #heading>ARMY</template>
+
+    Saya merupakan salah satu fans BTS, nama fanbase BTS bernama army. Army merupakan nama fanbase BTS yang identik dengan warna unggu, biasa dinamakan BORAHE. Borahae berasal dari bahasa Korea, yakni kata "bora" yang berarti warna ungu dan "hae" yang artinya melakukan. Oleh karenanya, dalam bahasa Inggris, "borahae" sama dengan "I purple you". Kata "borahae" dipakai sebagai pengganti kata "love" saat mengucapkan "I love you". Ketika army menonton konser biasanya mereka membawa lighstik, lightstik merupakan stik yang umumnya berukuran 15 cm dengan berbagai bentuk dan macam yang merepresentasikan makna dari grup idola dan fandom mereka. Dalam fanbase bts lightstik disebut sebagai AMYBOM. Lampu yang berbentuk bulat bertangkai yang bisa berubah warna.
+    <a href="https://www.youtube.com/watch?v=MXzHddgrNYQ" target="_blank" rel="noopener">AMYBOM</a>. Merupakan lightstik BTS versi terbaru.
+    <a href="https://www.youtube.com/watch?v=vkRKZLUwWkc" target="_blank" rel="noopener">AMYBOM COLOR</a>. contoh amybom ketika di bawa konser.
+  </WelcomeItem>
+</template>
